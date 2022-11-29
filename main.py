81e97bbbcc488ad46d774ee84d1599dfec499f40
lena moskovskikh <lena.moskovskikh@gmail.com>
2022-11-29 23:00:33 +0300

    первое задание

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+a = None
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+import sqlite3
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Widget(QMainWindow):
+    def __init__(self):
+        global a
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.con = sqlite3.connect('coffee.db')
+        self.cur = self.con.cursor()
+        self.new()
+        a = self
 
 
-# Press the green button in the gutter to run the script.
+    def new(self):
+        result = self.cur.execute(f"""SELECT * FROM type""").fetchall()
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setHorizontalHeaderLabels(['id', 'sort', 'roast', 'type', 'taste', 'price', 'amount'])
+        #объем в граммах
+        #цена в рублях
+
+        for value, item in enumerate(result):
+            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+            for el, row in enumerate(item):
+                self.tableWidget.setItem(value, el, QTableWidgetItem(str(row)))
+
+
+
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    ex = Widget()
+    ex.show()
+    sys.exit(app.exec_())
